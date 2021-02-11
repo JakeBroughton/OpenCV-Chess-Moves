0f47f9006cd9f93bbd7fa19f087b271b0d46cf38
JakeBroughton <46027440+JakeBroughton@users.noreply.github.com>
2021-02-11 02:27:34 +0000

    testing

--- ProjectFolder/openCV_Testing.py
+++ ProjectFolder/openCV_Testing.py
@@ -1,52 +1,27 @@
-# Ideas
-# Detect chess moves through camera
-
 import numpy as np
 import cv2
 
-filename = "testimage.png"
-img = cv2.imread(filename)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# filename = "test_images/blank_board.jpg"
+filename = 'test_images/real_board.jpg'
 
-gray = np.float32(gray)
+# Base image
+imgBase = cv2.imread(filename)
+
+cv2.imshow("Base Image", imgBase)
+
+# Edge detection
+img = cv2.imread(filename)
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Make grayscale
+
+gray = np.float32(gray)  # Corner harris method needs float32 format
+
 dst = cv2.cornerHarris(gray, 2, 3, 0.04)  # img, blockSize, ksize, k
 
 dst = cv2.dilate(dst, None)
 
-img[dst>0.01*dst.max()]=[0,0,255]
+img[dst > 0.01*dst.max()] = [0, 0, 255]
 
-cv2.imshow('dst',img)
+cv2.imshow('Harris edge detection', img)
 if cv2.waitKey(0) & 0xff == 27:
     cv2.destroyAllWindows()
-
-"""
-img = np.zeros((512,512,3), np.uint8)
-
-img = cv2.line(img, (0, 0), (511, 511), (255, 0, 0), 5)
-
-cv2.imshow("frame", img)
-if cv2.waitKey(0) == 27:
-    cv2.destroyAllWindows()
-
-
-img = cv2.imread('testimage.png', 0)
-cv2.imshow('image', img)
-k = cv2.waitKey(0)
-if k == 27: # Escape key
-    cv2.destroyAllWindows()
-
-cap = cv2.VideoCapture(0)
-
-while True:
-    # Capture frames
-    ret, frame = cap.read()
-
-    # Operations on the frame
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the result
-    cv2.imshow('frame', gray)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-"""
