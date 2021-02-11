70c0faf322617383a4b3e61e60fb7b8371c00f62
JakeBroughton <46027440+JakeBroughton@users.noreply.github.com>
2021-02-11 15:42:42 +0000

    Update main.py

--- ProjectFolder/main.py
+++ ProjectFolder/main.py
@@ -3,42 +3,61 @@ import cv2
 import numpy as np
 
 # Parameters
-windowH = 450
-windowW = 450
+windowH, windowW = 450, 450
 
-x_squares = 8
-y_squares = 7
+circles = np.zeros((4, 2), np.int)
 
-x_no = x_squares - 1
-y_no = y_squares - 1
+counter = 0
+
+
+def mousepoints(event, x, y, flags, params):
+    global counter
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        circles[counter] = x, y
+        counter += 1
+        print(circles)
+
 
 # Load base test image, to be later changed to a video source #
-#filename = 'test_images/blank_board.jpg'
-filename = 'test_images/calibration.jpg'
-
+# filename = 'test_images/blank_board.jpg'
+filename = 'test_images/real_board_2.jpg'
 source = cv2.imread(filename)
-# cv2.imshow("Base Image", source)
 
-base = cv2.resize(source, (windowH, windowW))
-cv2.imshow("Resize", base)
 
-# Find contours of image and draw #
-gray = cv2.cvtColor(base, cv2.COLOR_BGR2GRAY)
+cornerClick = cv2.resize(source, (windowH, windowW))
+cv2.imshow("CornerClick", cornerClick)
 
-# Find chessboard corners
-ret, corners = cv2.findChessboardCorners(gray, (x_no, y_no), None)
-if ret:
-    print("Found corners")
-    cv2.drawChessboardCorners(gray, (7,6), corners, ret)
-    cv2.imshow("image", gray)
+base = cornerClick.copy()
 
-    print(type(corners))
-    print(len(corners))
-    print(corners)
-# Warp image perspective #
+
+cv2.imshow("CornerClick", cornerClick)
+
+
+print("Double click corners of board.")
+
+while True:
+    if counter == 4:
+        # TODO: use myfuncs.py to sort corners into correct order (top left, top right, bottom left .etc)
+
+        # Warp image perspective #
+        pos1 = np.float32([circles[0], circles[1], circles[2], circles[3]])
+        pos2 = np.float32([[0, windowH], [windowW, windowH], [0, 0], [windowW, 0]])
+        matrix = cv2.getPerspectiveTransform(pos1, pos2)
+        imgOutput = cv2.warpPerspective(base, matrix, (windowW, windowH))
+        break
+
+    for x in range(0, 4):
+        cv2.circle(cornerClick, (circles[x][0], circles[x][1]), 5, (0, 255, 0), cv2.FILLED)
+    cv2.imshow("CornerClick", cornerClick)
+    # Look for double clicks on "base" window
+
+    cv2.setMouseCallback('CornerClick', mousepoints)
+
+    cv2.waitKey(1)
+
+cv2.imshow("Fixed Perspective", imgOutput)
+cv2.waitKey(0)
 
 # Split board into grid #
 
-# Wait for ESC key
-if cv2.waitKey(0) == 27:
-    cv2.destroyAllWindows()
+# Close all windows on ESC key
