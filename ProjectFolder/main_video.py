9130ba29c7efb2d23ab0800852c0aa031c61acfd
JakeBroughton <46027440+JakeBroughton@users.noreply.github.com>
2021-02-12 19:41:16 +0000

    Added main_video.py for testing

---
+++ ProjectFolder/main_video.py
@@ -0,0 +1,63 @@
+# Imports
+from myfuncs import *
+import cv2
+import numpy as np
+
+# Parameters
+windowH, windowW = 480, 480
+circles = np.zeros((4, 2), np.int)
+counter = 0
+
+filename = 'test_images/alex_nocamera.jpg'
+cap = cv2.VideoCapture(0)
+
+
+# Mouse click function
+def mousepoints(event, x, y, flags, params):
+    global counter
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        circles[counter] = x, y
+        counter += 1
+        # print(circles)
+
+
+# Initial video capture, press n to go next
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (windowW, windowH))
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        break
+    pass
+
+# Click corners and warp perspective
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (windowW, windowH))
+    cv2.imshow('frame', frame)
+    
+    if counter == 4:
+        # TODO: use myfuncs.py to sort corners into correct order (top left, top right, bottom left .etc)
+        order(circles)
+        # Warp image perspective #
+        pos1 = np.float32(order(circles))
+        pos2 = np.float32([[0, 0], [windowW, 0], [0, windowH], [windowW, windowH]])
+        matrix = cv2.getPerspectiveTransform(pos1, pos2)
+        imgOutput = cv2.warpPerspective(base, matrix, (windowW, windowH))
+        break
+
+    for x in range(0, 4):
+        cv2.circle(cornerClick, (circles[x][0], circles[x][1]), 5, (0, 255, 0), cv2.FILLED)
+
+
+    cv2.imshow("CornerClick", frame)
+    # Look for double clicks on "base" window
+    cv2.setMouseCallback('CornerClick', mousepoints)
+    cv2.waitKey(1)
+cv2.destroyWindow("CornerClick")
+cv2.imshow("Fixed Perspective", imgOutput)
+
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
